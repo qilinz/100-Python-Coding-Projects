28633f6f5e92d07f88ee2a76ac79aeb0dda2ef2a
Qilin Zhang <zhangqilinz@outlook.com>
2021-11-14 17:00:47 +0100

    Update Day14 project

--- Beginner/Day14-higher-lower/Day14-higher-lower.py
+++ Beginner/Day14-higher-lower/Day14-higher-lower.py
@@ -6,27 +6,43 @@ from gamedata import data
 
 
 # Functions
-# 1. select a person from the list and then remove it
 def random_select():
+    """
+    Select a person from the list and then remove it
+    :return: the person info dict
+    """
     selected = random.choice(data_list)
     data_list.remove(selected)
     return selected
 
 
-# 2. present info of a person
 def present_info(person):
+    """
+    Present info of the "person"
+    :param person: any dict from the list
+    :return: a string describing the person
+    """
     return f"{person['name']}, a {person['description']}, from {person['country']}."
 
 
-# 3. pose the question
 def pose_question(person_A, person_B):
+    """
+    Pose the question
+    :param person_A: the first person to compare
+    :param person_B: the second person to compare
+    """
     print(f"Compare A: {present_info(person_A)}")
     print(vs)
     print(f"Against B: {present_info(person_B)}")
 
 
-# 4. Compare the followers of two people and return the right answer
 def right_answer(person_A, person_B):
+    """
+    Compare the followers of two people and return the right answer
+    :param person_A: the first person to compare
+    :param person_B: the second person to compare
+    :return: string "A" or "B" representing the first person or the second one
+    """
     if person_A['follower_count'] > person_B['follower_count']:
         return 'A'
     else:
