f13702461c3c412443eecf9fb7a4715960a1ecde
Qilin Zhang <zhangqilinz@outlook.com>
2021-11-25 22:30:13 +0100

    Update Day26 project

--- Intermediate/Day26-nato-phonetic/main.py
+++ Intermediate/Day26-nato-phonetic/main.py
@@ -6,8 +6,17 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 # practicing dict comprehension instead of using to_dict()
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-# create the interface of the game
-word = input("Enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in word]
 
-print(output_list)
+# create the interface of the game
+def nato():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato()
+    else:
+        print(output_list)
+
+
+nato()
