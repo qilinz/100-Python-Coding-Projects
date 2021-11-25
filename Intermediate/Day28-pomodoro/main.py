7583a657497de0f456e50f36c477c12ea3037721
Qilin Zhang <zhangqilinz@outlook.com>
2021-11-25 17:18:10 +0100

    Update Day28 project

--- Intermediate/Day28-pomodoro/main.py
+++ Intermediate/Day28-pomodoro/main.py
@@ -89,15 +89,15 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 # make the countdown time a variable to be accessed elsewhere
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 34, "bold"))
 canvas.grid(column=2, row=2)
 
 # start button
-start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button = Button(text="Start", font=(FONT_NAME, 16), highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=3)
 
 # reset button
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button = Button(text="Reset", font=(FONT_NAME, 16), highlightthickness=0, command=reset_timer)
 reset_button.grid(column=3, row=3)
 
 # check mark
